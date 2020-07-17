038d52499824f3ccbd8eac501a0bc754a0eb5c8f
kongqiahaha <32472226+kongqiahaha@users.noreply.github.com>
2020-07-17 18:43:07 +0800

    debug

--- blog/static/js/index.js
+++ blog/static/js/index.js
@@ -1,11 +1,10 @@
 
 
 function goto_Twitter() {
-    open("https://twitter.com/GBe4yGzTAdE0xjb")
+    open("")
 
 }
 function goto_Facebook() {
-    open("https://weibo.com/5762683437/profile?topnav=1&wvr=6&is_all=1")
+    open("")
 
 }
-
