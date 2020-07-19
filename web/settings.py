fa855e01f5773e70f9739166e9292ec33f5b5c7c
kongqiahaha <32472226+kongqiahaha@users.noreply.github.com>
2020-07-19 11:20:45 +0800

    debug

--- web/settings.py
+++ web/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER':'testuser',
+        'PASSWORD':'il0ve&Sql',
+        'HOST':'114.55.252.152',
+        'POST':'3306',
     }
 }
 
